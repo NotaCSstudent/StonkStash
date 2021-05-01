16c9bf050804723f256d562eba8baedc7996f94f
Embee <embee@Embees-MBP.fios-router.home>
2021-05-01 11:46:50 -0400

    some deletions

--- stonks_frontend/src/pages/Landing.js
+++ stonks_frontend/src/pages/Landing.js
@@ -26,8 +26,8 @@ class Landing extends Component {
           <p id="mission_text" className="light">Investing in the stock market has been localized with free platforms like Robinhood and WeBull therefore, this advancement 
 removed any barrier of entry into the stockmarket “no matter how much experience you have (or don’t have)” (Robinhood). This may 
 sound idealistic however, we emphasize the economic principle of intentions versus effects. A staggering 90% of hopeful and 
-seasoned investors lose money in the stock market; these apps removing any barrier to entry have only exacerbated that statistic. {this.data}
-Investing is no get rich quick scheme but can ensure a prosperous future if approached with the correct mindset and strategy. 
+seasoned investors lose, money in the stock market; these apps removing any barrier to entry have only exacerbated that statistic. {this.data}
+Investing is no get, rich quick scheme but can ensure a prosperous future if approached with the correct mindset and strategy. 
 Investing in the stock market is not hard; like most things, it entails a learning curve. Stonkstache is here to walk you through the 
 learning curve and serve as your educational tool to understanding the principles and mindset of successful investors.</p>
         </Row>
