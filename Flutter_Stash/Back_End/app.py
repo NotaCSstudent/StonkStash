608c0cfba3af7eac823226d50820d77616bae067
NotaCSstudent <mahmudbabu94@gmail.com>
2021-05-29 15:46:14 -0400

    added some networking tools to connect flask with flutter

--- Flutter_Stash/Back_End/app.py
+++ Flutter_Stash/Back_End/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask,jsonify
 
+hello = "{}".format(3)
+bye = "{}".format(2)
 
 
 app = Flask(__name__)
+#Route '/' to facilitate get request from our flutter app
+@app.route('/', methods = ['GET'])
+def index():
+    return jsonify({'greetings' : 'Hi! this is python'}) #returning key-value pair in json format
 
 
-@app.route('/')
-def hello_world():
-    return "Hello"
+if __name__ == "__main__":
+    app.run(debug = True) #debug will allow changes without shutting down the server 
+
+
